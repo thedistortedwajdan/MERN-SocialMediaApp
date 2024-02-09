1eed5fb5cababd3071c65d555a794da3f6da533d
thedistortedwajdan <wajdan.mohammad@mail.com>
2024-02-09 13:36:33 +0500

    created follow api

--- backend/api/controller/userController.js
+++ backend/api/controller/userController.js
@@ -46,4 +46,30 @@ const get = async (req, res) => {
   }
 };
 
-export { update, remove, get };
+const follow = async (req, res) => {
+  try {
+    if (req.body.id === req.params.id) {
+      return res.status(400).json("cannot follow yourself");
+    }
+    const followed = await userModel.findById(req.params.id);
+    if (!followed) {
+      return res.status(400).json("followed does not exist");
+    }
+    const follower = await userModel.findById(req.body.id);
+    if (!follower) {
+      return res.status(400).json("follower does not exist");
+    }
+
+    if (followed.followers.includes(follower._doc._id)) {
+      return res.status(400).json("already following");
+    }
+    await followed.updateOne({ $push: { followers: follower._doc._id } });
+    await follower.updateOne({ $push: { following: followed._doc._id } });
+    return res.status(200).json("followed");
+  } catch (error) {
+    console.log(`follow => ${error.message}`);
+    return res.status(500).json("server error");
+  }
+};
+
+export { update, remove, get, follow };
--- backend/api/userApi.js
+++ backend/api/userApi.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { get, remove, update } from "./controller/userController.js";
+import { follow, get, remove, update } from "./controller/userController.js";
 
 const router = Router();
 
 router.put("/:id", update);
 router.delete("/:id", remove);
 router.get("/:id", get);
+
+router.put("/follow/:id", follow);
 
 export default router;
