b3452574781d5365f6b3f0c49198c0db1a313171
thedistortedwajdan <wajdan.mohammad@gmail.com>
2024-02-17 00:51:12 +0500

    feb 17 24, Rightbar birthday & online friends basic

--- frontend/src/components/Rightbar.jsx
+++ frontend/src/components/Rightbar.jsx
@@ -1,17 +1,223 @@
-import { Box } from "@mui/material";
+import {
+  Avatar,
+  Box,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Paper,
+  Typography,
+} from "@mui/material";
+import CakeIcon from "@mui/icons-material/Cake";
 
 export default function Rightbar() {
   return (
     <>
       <Box
         flex={2.5}
-        bgcolor={"blue"}
         display={{
           xs: "none",
           sm: "block",
         }}
+        sx={{ maxHeight: "93vh", overflowY: "scroll" }}
       >
-        Rightbar
+        <Box
+          sx={{
+            p: 1,
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <Paper
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              p: 1,
+              my: 1,
+              cursor: "pointer",
+              "&:hover": {
+                bgcolor: "#f6fafe",
+              },
+            }}
+          >
+            <CakeIcon
+              sx={{
+                mr: 1,
+                color: "red",
+              }}
+            />
+            <Typography>
+              <Typography component={"span"} fontWeight={"bold"}>
+                Jane Dane
+              </Typography>{" "}
+              and{" "}
+              <Typography component={"span"} fontWeight={"bold"}>
+                3 others
+              </Typography>{" "}
+              have birthday today.
+            </Typography>
+          </Paper>
+          <Box sx={{ bgcolor: "background.paper" }}>
+            <nav aria-label="secondary mailbox folders">
+              <List>
+                <ListItem alignItems="flex-start">
+                  <ListItemAvatar>
+                    <Avatar alt="Remy Sharp" src="" />
+                  </ListItemAvatar>
+                  <ListItemText
+                    sx={{
+                      alignSelf: "center",
+                    }}
+                    primary="Ali Conors"
+                  />
+                </ListItem>
+                <ListItem alignItems="flex-start">
+                  <ListItemAvatar>
+                    <Avatar alt="Remy Sharp" src="" />
+                  </ListItemAvatar>
+                  <ListItemText
+                    sx={{
+                      alignSelf: "center",
+                    }}
+                    primary="Ali Conors"
+                  />
+                </ListItem>
+                <ListItem alignItems="flex-start">
+                  <ListItemAvatar>
+                    <Avatar alt="Remy Sharp" src="" />
+                  </ListItemAvatar>
+                  <ListItemText
+                    sx={{
+                      alignSelf: "center",
+                    }}
+                    primary="Ali Conors"
+                  />
+                </ListItem>
+                <ListItem alignItems="flex-start">
+                  <ListItemAvatar>
+                    <Avatar alt="Remy Sharp" src="" />
+                  </ListItemAvatar>
+                  <ListItemText
+                    sx={{
+                      alignSelf: "center",
+                    }}
+                    primary="Ali Conors"
+                  />
+                </ListItem>
+                <ListItem alignItems="flex-start">
+                  <ListItemAvatar>
+                    <Avatar alt="Remy Sharp" src="" />
+                  </ListItemAvatar>
+                  <ListItemText
+                    sx={{
+                      alignSelf: "center",
+                    }}
+                    primary="Ali Conors"
+                  />
+                </ListItem>
+                <ListItem alignItems="flex-start">
+                  <ListItemAvatar>
+                    <Avatar alt="Remy Sharp" src="" />
+                  </ListItemAvatar>
+                  <ListItemText
+                    sx={{
+                      alignSelf: "center",
+                    }}
+                    primary="Ali Conors"
+                  />
+                </ListItem>
+                <ListItem alignItems="flex-start">
+                  <ListItemAvatar>
+                    <Avatar alt="Remy Sharp" src="" />
+                  </ListItemAvatar>
+                  <ListItemText
+                    sx={{
+                      alignSelf: "center",
+                    }}
+                    primary="Ali Conors"
+                  />
+                </ListItem>
+                <ListItem alignItems="flex-start">
+                  <ListItemAvatar>
+                    <Avatar alt="Remy Sharp" src="" />
+                  </ListItemAvatar>
+                  <ListItemText
+                    sx={{
+                      alignSelf: "center",
+                    }}
+                    primary="Ali Conors"
+                  />
+                </ListItem>
+                <ListItem alignItems="flex-start">
+                  <ListItemAvatar>
+                    <Avatar alt="Remy Sharp" src="" />
+                  </ListItemAvatar>
+                  <ListItemText
+                    sx={{
+                      alignSelf: "center",
+                    }}
+                    primary="Ali Conors"
+                  />
+                </ListItem>
+                <ListItem alignItems="flex-start">
+                  <ListItemAvatar>
+                    <Avatar alt="Remy Sharp" src="" />
+                  </ListItemAvatar>
+                  <ListItemText
+                    sx={{
+                      alignSelf: "center",
+                    }}
+                    primary="Ali Conors"
+                  />
+                </ListItem>
+                <ListItem alignItems="flex-start">
+                  <ListItemAvatar>
+                    <Avatar alt="Remy Sharp" src="" />
+                  </ListItemAvatar>
+                  <ListItemText
+                    sx={{
+                      alignSelf: "center",
+                    }}
+                    primary="Ali Conors"
+                  />
+                </ListItem>
+                <ListItem alignItems="flex-start">
+                  <ListItemAvatar>
+                    <Avatar alt="Remy Sharp" src="" />
+                  </ListItemAvatar>
+                  <ListItemText
+                    sx={{
+                      alignSelf: "center",
+                    }}
+                    primary="Ali Conors"
+                  />
+                </ListItem>
+                <ListItem alignItems="flex-start">
+                  <ListItemAvatar>
+                    <Avatar alt="Remy Sharp" src="" />
+                  </ListItemAvatar>
+                  <ListItemText
+                    sx={{
+                      alignSelf: "center",
+                    }}
+                    primary="Ali Conors"
+                  />
+                </ListItem>
+                <ListItem alignItems="flex-start">
+                  <ListItemAvatar>
+                    <Avatar alt="Remy Sharp" src="" />
+                  </ListItemAvatar>
+                  <ListItemText
+                    sx={{
+                      alignSelf: "center",
+                    }}
+                    primary="Ali Conors"
+                  />
+                </ListItem>
+              </List>
+            </nav>
+          </Box>
+        </Box>
       </Box>
     </>
   );
